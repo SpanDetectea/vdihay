6e222a4e3c359d35074d62b03069eec719755e30
SpanDetectea <SpanDetectea@gmail.com>
2025-04-12 18:42:56 +0300

    reservation starting form

--- src/Components/Main/Reservation/Reservation.jsx
+++ src/Components/Main/Reservation/Reservation.jsx
@@ -1,9 +1,33 @@
-import './Reservation.scss'
+import { useState } from "react";
+import "./Reservation.scss";
 
 function Reservation() {
-    return ( <div className='reservation'>
+    const [date, setDate] = useState("")
+    const handleDate = (e) => {
+        setDate(e.target.value)
+    }
+  return (
+    <div className="reservation">
+      <form action="" className="reservation__form">
+        <div className="reservation__form__date">
+          <input type="date" className={`reservation__form__date-input reservation__form-input ${date != '' ? 'has-value': ''}`} placeholder="Выберите день" onChange={handleDate} value={date}/>
+          <input type="time" className="reservation__form__date-input reservation__form-input" />
+          <input type="time" className="reservation__form__date-input reservation__form-input" />
+        </div>
 
-    </div> );
+        <input
+          type="number"
+          className="reservation__form-input"
+          placeholder="Количество человек"
+        />
+        <input
+          type="text"
+          className="reservation__form-input"
+          placeholder="Комментарии"
+        />
+      </form>
+    </div>
+  );
 }
 
 export default Reservation;
